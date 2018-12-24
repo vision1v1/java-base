697015f3a5ab692ef9875eca3309c57d6b6e2dfa
sunsiyuan <sun.siyuan@dr-elephant.com>
2018-12-24 15:09:34 +0800

    add hash map

--- java-data-structure/java-array-list/src/main/java/javaarraylist/JavaArrayListMain.java
+++ java-data-structure/java-array-list/src/main/java/javaarraylist/JavaArrayListMain.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class JavaArrayListMain {
     public static void main(String[] args){
+
+
         //testArrayListSubListIssue();
 
         testFixArrayListSubListIssue();
---
+++ java-data-structure/java-hash-map/src/main/java/javahashmap/JavaHashMapMain.java
@@ -0,0 +1,17 @@
+package javahashmap;
+
+import java.util.HashMap;
+
+public class JavaHashMapMain {
+    public static void main(String[] args){
+        test01();
+    }
+
+    public static void test01(){
+        HashMap<String,Integer> ageMaps = new HashMap<String, Integer>();
+        ageMaps.put("infi",20);
+        ageMaps.put("th000",22);
+
+
+    }
+}
