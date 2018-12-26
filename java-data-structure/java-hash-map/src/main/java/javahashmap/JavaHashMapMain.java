8a340472ef0e3447a38253046d7ea5e6a4916c20
sunsiyuan <sun.siyuan@dr-elephant.com>
2018-12-26 17:57:47 +0800

    Difference between Synchronized HashMap and ConcurrentHashMap

--- java-data-structure/java-hash-map/src/main/java/javahashmap/JavaHashMapMain.java
+++ java-data-structure/java-hash-map/src/main/java/javahashmap/JavaHashMapMain.java
@@ -1,7 +1,10 @@
 package javahashmap;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.BiConsumer;
 
 
@@ -12,7 +15,19 @@ public class JavaHashMapMain {
 
     public static void main(String[] args) {
 
-        testMap();
+        //testMap();
+
+        //ConcurrentHashMap 与 SynchronizedMap 区别
+
+        //1.多个线程可以从ConcurrentHashMap中删除键值对, 而在SynchronizedMap情况下, 只允许有一个线程进行更改。这将导致在ConcurrentHashMap实现更高的并发性。
+
+        //2.无需锁定map即可读取ConcurrentHashMap中的值。检索操作将返回由最近完成的插入操作插入的值。在SynchronizedMap中, 读取操作也需要锁。
+
+        //3.如果一个线程尝试修改它, 而另一个线程在它上迭代, ConcurrentHashMap 不会引发并发修改异常。迭代器反映hash map创建时的状态。SynchronizedMap返回迭代器, 它在并发修改时会立即失败。
+
+        testConcurrentHashMap();
+
+        testSynchronizedMap();
 
     }
 
@@ -41,11 +56,69 @@ public class JavaHashMapMain {
             }
         });
 
+        System.out.println("-----------------iterator----------");
+
+        Iterator<String> keyIterator = ageMaps.keySet().iterator();
+        while (keyIterator.hasNext()){
+            String key = keyIterator.next();
+            System.out.println(key + " : " + ageMaps.get(key));
+        }
+
+        System.out.println("-----------------iterator forEachRemaining----------");
+
+        keyIterator = ageMaps.keySet().iterator();
+
+        keyIterator.forEachRemaining(item->{
+            System.out.println(item + " : " + ageMaps.get(item));
+        });
+
+        System.out.println("-----------------get----------");
+
         System.out.println(ageMaps.get("th000"));
         System.out.println(ageMaps.get("fly100"));
         System.out.println(ageMaps.get("ted"));
 
     }
+
+    public static void testConcurrentHashMap(){
+        ConcurrentHashMap<String,Integer> ageMaps = new ConcurrentHashMap<>();
+        ageMaps.put("infi", 20);
+        ageMaps.put("th000", 22);
+        ageMaps.put("fly100", 23);
+        ageMaps.put("ted", 24);
+
+        Iterator<String> iterator = ageMaps.keySet().iterator();
+
+        synchronized (ageMaps){
+            while (iterator.hasNext()){
+                String key = iterator.next();
+                System.out.println(key + " : " + ageMaps.get(key));
+            }
+        }
+    }
+
+
+    public static void testSynchronizedMap(){
+        Map<Integer, String> syncHashMap = Collections.synchronizedMap(new HashMap<>());
+
+        //Put require no synchronization
+        syncHashMap.put(1, "A");
+        syncHashMap.put(2, "B");
+
+        //Get require no synchronization
+        syncHashMap.get(1);
+
+        Iterator<Integer> itr = syncHashMap.keySet().iterator();
+
+        //Using synchronized block is advisable
+        synchronized (syncHashMap)
+        {
+            while(itr.hasNext()) {
+                System.out.println(syncHashMap.get(itr.next()));
+            }
+        }
+    }
+
 
 
     static final int hash(Object key) {
