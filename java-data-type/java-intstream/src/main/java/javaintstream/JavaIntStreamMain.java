a900ddf6fc30a147741159a0ceacd0a445eec0be
sunsiyuan <sun.siyuan@dr-elephant.com>
2019-01-22 18:11:49 +0800

    优化代码

--- java-data-type/java-intstream/src/main/java/javaintstream/JavaIntStreamMain.java
+++ java-data-type/java-intstream/src/main/java/javaintstream/JavaIntStreamMain.java
@@ -1,12 +1,12 @@
 package javaintstream;
 
-import java.util.Arrays;
-import java.util.Spliterator;
-import java.util.Spliterators;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.IntConsumer;
 import java.util.function.Supplier;
 import java.util.stream.*;
+
+import static java.util.stream.IntStream.builder;
 
 public class JavaIntStreamMain {
     public static void main(String[] args) {
@@ -16,6 +16,10 @@ public class JavaIntStreamMain {
         test04();
         test05();
         test06();
+        test07();
+        test08();
+        test09();
+        test10();
     }
 
     public static void test01() {
@@ -125,13 +129,144 @@ public class JavaIntStreamMain {
         Spliterator.OfInt spliterator = Arrays.spliterator(ints, 0, ints.length);
         IntStream intStream = StreamSupport.intStream(spliterator, false);
         intStream = intStream.filter(e -> {
-            return e > 2;
+            return e >= 2;
         }).filter(e -> e < 4);
         //intStream.forEach(JavaIntStreamMain::print);
+
         int[] result = intStream.toArray();
+        //int[] result2 = intStream.toArray();//stream has already been operated upon or closed
         printIntArray(result);
         System.out.println();
     }
+
+    @SuppressWarnings("Duplicates")
+    public static void test07() {
+        int[] ints = new int[]{1, 2, 3, 4, 5};
+        Spliterator.OfInt spliterator = Arrays.spliterator(ints, 0, ints.length);
+        IntStream intStream = StreamSupport.intStream(spliterator, false);
+        intStream = intStream.map(a -> {
+            return a + 1;
+        });
+        int[] result = intStream.toArray();
+        //int[] result2 = intStream.toArray();//stream has already been operated upon or closed
+        printIntArray(result);
+        System.out.println();
+    }
+
+    @SuppressWarnings("Duplicates")
+    public static void test08() {
+
+        class Student {
+
+            Student(int id) {
+                this.Id = id;
+            }
+
+            public int getId() {
+                return Id;
+            }
+
+            public void setId(int id) {
+                Id = id;
+            }
+
+            @Override
+            public String toString() {
+                return "StudentId : " + Id;
+            }
+
+            private int Id;
+        }
+
+        int[] ints = new int[]{1, 2, 3, 4, 5};
+        Spliterator.OfInt spliterator = Arrays.spliterator(ints, 0, ints.length);
+        IntStream intStream = StreamSupport.intStream(spliterator, false);
+        Stream<Student> stream = intStream.mapToObj(i -> {
+            return new Student(i);
+        });
+        List<Student> studentList = stream.collect(Collectors.toList());
+        //List<Student> studentList2 = stream.collect(Collectors.toList());//stream has already been operated upon or closed
+        studentList.forEach(s -> {
+            System.out.println(s);
+        });
+        System.out.println();
+    }
+
+    public static void test09() {
+
+        class Student {
+
+            Student(int id, String name, boolean sex) {
+                this.Id = id;
+                this.sex = sex;
+                this.name = name;
+            }
+
+            public int getId() {
+                return Id;
+            }
+
+            public void setId(int id) {
+                Id = id;
+            }
+
+            @Override
+            public String toString() {
+                return "[StudentId : " + Id + " Name : " + name + " Sex : " + sex + "]";
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+
+            public boolean isSex() {
+                return sex;
+
+            }
+
+            public void setSex(boolean sex) {
+                this.sex = sex;
+            }
+
+
+
+            private int Id;
+            private boolean sex;
+            private String name;
+        }
+
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "infi", true));
+        students.add(new Student(2, "th000", false));
+        students.add(new Student(3, "fly", true));
+        students.add(new Student(4, "lyn", true));
+        students.add(new Student(5, "moon", false));
+
+        List<Student> studentList = students.stream().filter(student -> student.sex == true).filter(student -> student.Id > 2).collect(Collectors.toList());
+
+        studentList.forEach(student -> System.out.println(student));
+    }
+
+    public static void test10(){
+        int[] ints = new int[]{1, 2, 3, 4, 5};
+        IntStream stream = Arrays.stream(ints);
+        int[] result = stream.flatMap(e->IntStream.of(e+e)).toArray();
+        printIntArray(result);
+    }
+
+//    public static void test11(){
+//        int[] ints1 = new int[]{1, 2, 3, 4, 5};
+//        IntStream stream1 = Arrays.stream(ints1);
+//        int[] ints2 = new int[]{-1, -2, -3, -4, -5};
+//        IntStream stream2 = Arrays.stream(ints2);
+//
+//        int[] result = stream1.flatMap(e-> stream2.map(s->s+e)).toArray();
+//        printIntArray(result);
+//    }
 
 
     private static void print(int i) {
