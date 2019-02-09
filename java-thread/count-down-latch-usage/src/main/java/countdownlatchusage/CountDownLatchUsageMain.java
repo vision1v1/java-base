7955ecca4f3798b704d6977ee93b37a4c12b7440
vision1v1 <vision1v1@163.com>
2019-02-09 10:51:38 +0800

    add count-down-latch-usage

---
+++ java-thread/count-down-latch-usage/src/main/java/countdownlatchusage/CountDownLatchUsageMain.java
@@ -0,0 +1,55 @@
+package countdownlatchusage;
+
+import java.util.concurrent.CountDownLatch;
+
+public class CountDownLatchUsageMain {
+
+    static class Worker extends Thread {
+
+        private CountDownLatch countDownLatch;
+        private int delay;
+
+        Worker(int delay, CountDownLatch countDownLatch) {
+            this.countDownLatch = countDownLatch;
+            this.delay = delay;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(delay);
+                countDownLatch.countDown();
+                System.out.println(Thread.currentThread().getName() + " Done.");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try{
+            CountDownLatch countDownLatch = new CountDownLatch(4);
+
+            Worker w1 = new Worker(100000, countDownLatch);
+            w1.setName("w1");
+            Worker w2 = new Worker(200000, countDownLatch);
+            w2.setName("w2");
+            Worker w3 = new Worker(300000, countDownLatch);
+            w3.setName("w3");
+            Worker w4 = new Worker(400000, countDownLatch);
+            w4.setName("w4");
+
+            w1.start();
+            w2.start();
+            w3.start();
+            w4.start();
+
+            countDownLatch.await();
+
+            System.out.println(Thread.currentThread().getName() + " finished");
+        }
+        catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
