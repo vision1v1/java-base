a98d43200d30797523674c96112f6bf15a02414b
vision1v1 <vision1v1@163.com>
2019-03-05 12:14:08 +0800

    test blocking queue

---
+++ java-data-structure/java-blocking-queue/src/main/java/javablockingqueue/JavaBlockingQueueMain.java
@@ -0,0 +1,17 @@
+package javablockingqueue;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class JavaBlockingQueueMain {
+    public static void main(String[] args){
+        test01();
+    }
+
+    public static void test01(){
+        BlockingQueue<Integer> queue = new ArrayBlockingQueue(5);
+
+
+
+    }
+}
